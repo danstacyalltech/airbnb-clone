12dcebfa658811f292f5d6f0bd488104a70596f9
YOUR-USERNAME <dan.stacy@alltech.com>
2023-05-12 16:06:03 -0400

    Created input fields in SignUp Modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -9,6 +9,7 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
 import Heading from "../Heading";
+import Input from "../inputs/Input";
 
 interface RegisterModalProps {}
 
@@ -50,6 +51,31 @@ const RegisterModal = ({}: RegisterModalProps) => {
 				title="Welcome to Airbnb"
 				subtitle="Sign up to get started"
 				center
+			/>
+			<Input 
+				id="email"
+				label="Email"
+				disabled={isLoading}
+				register={register}
+				errors={errors}
+				required
+			/>
+			<Input 
+				id="name"
+				label="Name"
+				disabled={isLoading}
+				register={register}
+				errors={errors}
+				required
+			/>
+			<Input 
+				id="password"
+				type="password"
+				label="Password"
+				disabled={isLoading}
+				register={register}
+				errors={errors}
+				required
 			/>
 		</div>
 	);
